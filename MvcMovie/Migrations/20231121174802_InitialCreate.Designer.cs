3292f8ddc9497fd3e75bedaefdcc4ac04457d56e
milton.honji <factorysolutionIt@outlook.com>
2023-11-24 13:02:26 -0300

    Finalizado o tutorial de ASP.NET core MVC

--- MvcMovie/Migrations/20231121174802_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20231121174802_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20231109181927_InitialCreate")]
+    [Migration("20231121174802_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -24,6 +24,7 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
                     new Movie
@@ -31,6 +32,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
                     new Movie
@@ -38,6 +40,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
                     new Movie
@@ -45,6 +48,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
